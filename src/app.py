191fa87619c3eb7d359aee9126d2573f02d48b9d
holyfs <egcg1986@gmail.com>
2022-06-29 12:33:15 +0000

    entrega de la family API

--- src/app.py
+++ src/app.py
@@ -6,14 +6,13 @@ from flask import Flask, request, jsonify, url_for
 from flask_cors import CORS
 from utils import APIException, generate_sitemap
 from datastructures import FamilyStructure
-#from models import Person
 
 app = Flask(__name__)
 app.url_map.strict_slashes = False
 CORS(app)
 
 # create the jackson family object
-jackson_family = FamilyStructure("Jackson")
+jackson_family = FamilyStructure()
 
 # Handle/serialize errors like a JSON object
 @app.errorhandler(APIException)
@@ -27,18 +26,46 @@ def sitemap():
 
 @app.route('/members', methods=['GET'])
 def handle_hello():
-
     # this is how you can use the Family datastructure by calling its methods
     members = jackson_family.get_all_members()
     response_body = {
-        "hello": "world",
+        "msg": "ok",
         "family": members
     }
-
-
     return jsonify(response_body), 200
+
+@app.route('/members', methods=['POST'])
+def add_member():
+    body=request.get_json()
+    member = {
+        "first_name": body["first_name"],
+        "last_name":body["last_name"],
+        "age":body["age"],
+        "lucky_numbers":body["lucky_numbers"]
+    }
+    jackson_family.add_member(member)
+    return jsonify({"msg": "Member created!"}), 201
+
+@app.route('/members/<int:member_id>', methods=['GET'])
+def get_member_by_id(member_id):
+    member = jackson_family.get_member(member_id)
+    response_body = {
+        "msg": "ok",
+        "family": member
+    }
+    return jsonify(response_body), 200
+    
+@app.route('/members/<int:member_id>', methods=['DELETE'])
+def delete_member_by_id(member_id):
+    member = jackson_family.delete_member(member_id)
+    response_body = {
+        "msg": "ok"
+    }
+    return jsonify(response_body), 200
+
+
 
 # this only runs if `$ python src/app.py` is executed
 if __name__ == '__main__':
-    PORT = int(os.environ.get('PORT', 3000))
+    PORT = int(os.environ.get('PORT', 3001))
     app.run(host='0.0.0.0', port=PORT, debug=True)
--- src/datastructures.py
+++ src/datastructures.py
@@ -9,27 +9,54 @@ update this file to implement the following already declared methods:
 from random import randint
 
 class FamilyStructure:
-    def __init__(self, last_name):
-        self.last_name = last_name
+    def __init__(self):
+#        self.last_name = last_name
 
         # example list of members
-        self._members = []
+        self._members = [{
+            "id": self._generateId(),
+            "first_name": "John",
+            "last_name": "Jackson",
+            "age":33,
+            "lucky_numbers":[7, 13, 22]
+        },
+        {
+            "id": self._generateId(),
+            "first_name": "Jane",
+            "last_name": "Jackson",
+            "age":35,
+            "lucky_numbers":[10, 14, 3]
+        },
+        {
+            "id": self._generateId(),
+            "first_name": "Jimmy",
+            "last_name": "Jackson",
+            "age":5,
+            "lucky_numbers":[1]
+        }
+        ]
 
     # read-only: Use this method to generate random members ID's when adding members into the list
     def _generateId(self):
         return randint(0, 99999999)
 
     def add_member(self, member):
-        # fill this method and update the return
-        pass
+        member["id"] = self._generateId()
+        self._members.append(member)        
 
     def delete_member(self, id):
-        # fill this method and update the return
-        pass
+        member = self.get_member(id)
+        if member is None:
+            return None
+        self._members.remove(member)
 
     def get_member(self, id):
-        # fill this method and update the return
-        pass
+        resp = None
+        for member in self._members:
+            if member["id"]== id:
+                resp = member
+                break
+        return resp
 
     # this method is done, it returns a list with all the family members
     def get_all_members(self):
